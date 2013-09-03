22277066ea579673a96e2d8bb86c4569d3210c88
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2013-09-03 09:06:37 -0600

    Changes for the day

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ iteration
 
 While and For loops
 
-#Latest edit
+#HW
 
-##Last Edit
+Attempt to get java and git working at home,
+bring quiestions on Thursday if it fails.
+
+Chapter 9 - Problems 3 and 5 - synced to Github.
+
+Windows users download Github for Windows to use git.
+Also need to download and installjava jdk. 
+Command line: set path variable for java. Or Eclipse.
--- loops.java
+++ loops.java
@@ -2,6 +2,9 @@ public class loops {
 
 	public static void main(String[] args) {
 		
+		String s ="My String";
+		int length = findLength(s);
+		System.out.println(length);
 
 		// int x = 1; //start
 		// while(x <= 100){
@@ -9,9 +12,33 @@ public class loops {
 		// 	x++;
 		// }
 
-		for (int i=1; i <= 100; i++) {
-			System.out.println(i);
+		// for (int i=1; i <= 100; i++) {
+		// 	System.out.println(i);
+		// }
+
+		// int z = 45;
+
+		// while (z >= 20) {
+		// 	System.out.println(z);
+		// 	z-=5;
+		// }
+
+		// System.out.println();
+
+		// for (int i = 24; i<= 68; i+=2) {
+		// 	System.out.println(i);
+		// }
+
+	}
+
+	//return the length of str. assume no length method built in to String
+	public static int findLength(String str){
+		int len = 0;
+		for (int i=0; i<str.length; i++) {
+			len++;
 		}
+
+		return len;
 
 	}
 	
